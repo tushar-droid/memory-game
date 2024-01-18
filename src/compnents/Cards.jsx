890ca63e34ef4ee96cc3d1583b95f98d4d70a443
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2024-01-18 00:19:55 -0800

    image bug possible fix 3

--- src/compnents/Cards.jsx
+++ src/compnents/Cards.jsx
@@ -1,12 +1,28 @@
 import legendsArray from "./legends";
 import cardLogo from '../assets/images/back-logo.png'
+
+import ash from '../assets/images/ash.png'
+import ballistic from '../assets/images/ballistic.png'
+import bangalore from '../assets/images/bangalore.png'
+
+
 // eslint-disable-next-line react/prop-types
 const Cards = ({cardId, clickHandler}) =>{
+    const legendImages = {
+        'ash': ash,
+        'ballistic': ballistic,
+        'bangalore': bangalore
+    }
+
+
+
+
     const indx = legendsArray.findIndex((legend) => legend.id === cardId);
+
     return(
         <div className="legend-card flipped" onClick={e => clickHandler(e, indx)}>
             <div className="front card-face">
-            <img src = {'src/assets/images/'+legendsArray[indx].name+'.png'} className="card-image"/>
+            <img src = {legendImages[legendsArray[indx].name]} className="card-image"/>
 
                 {/* <img src = {legendsArray[indx].imgSrc} className="card-image"/> */}
                 <h1 className="legend-title">{legendsArray[indx].name}</h1>
