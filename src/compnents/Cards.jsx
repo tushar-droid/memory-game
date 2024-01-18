3b1aa0dafd7e84def74e690272bf38058829d5db
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2024-01-18 00:29:31 -0800

    Image bug in production build fixed

--- src/compnents/Cards.jsx
+++ src/compnents/Cards.jsx
@@ -4,6 +4,27 @@ import cardLogo from '../assets/images/back-logo.png'
 import ash from '../assets/images/ash.png'
 import ballistic from '../assets/images/ballistic.png'
 import bangalore from '../assets/images/bangalore.png'
+import bloodhound from '../assets/images/bloodhound.png'
+import catalyst from '../assets/images/catalyst.png'
+import caustic from '../assets/images/caustic.png'
+import conduit from '../assets/images/conduit.png'
+import crypto from '../assets/images/crypto.png'
+import fuse from '../assets/images/fuse.png'
+import gibraltar from '../assets/images/gibraltar.png'
+import horizon from '../assets/images/horizon.png'
+import lifeline from '../assets/images/lifeline.png'
+import loba from '../assets/images/loba.png'
+import madmaggie from '../assets/images/madmaggie.png'
+import mirage from '../assets/images/mirage.png'
+import newcastle from '../assets/images/newcastle.png'
+import octane from '../assets/images/octane.png'
+import pathfinder from '../assets/images/pathfinder.png'
+import rampart from '../assets/images/rampart.png'
+import revenant from '../assets/images/revenant.png'
+import seer from '../assets/images/seer.png'
+import valkyrie from '../assets/images/valkyrie.png'
+import vantage from '../assets/images/vantage.png'
+import wattson from '../assets/images/wattson.png'
 
 
 // eslint-disable-next-line react/prop-types
@@ -11,7 +32,28 @@ const Cards = ({cardId, clickHandler}) =>{
     const legendImages = {
         'ash': ash,
         'ballistic': ballistic,
-        'bangalore': bangalore
+        'bangalore': bangalore,
+        'bloodhound': bloodhound,
+        'catalyst':catalyst,
+        'caustic':caustic,
+        'conduit':conduit,
+        'crypto':crypto,
+        'fuse':fuse,
+        'gibraltar':gibraltar,
+        'horizon':horizon,
+        'lifeline':lifeline,
+        'loba':loba,
+        'madmaggie':madmaggie,
+        'mirage':mirage,
+        'newcastle':newcastle,
+        'octane':octane,
+        'pathfinder':pathfinder,
+        'rampart':rampart,
+        'revenant':revenant,
+        'seer':seer,
+        'valkyrie':valkyrie,
+        'vantage':vantage,
+        'wattson':wattson,
     }
 
 
@@ -23,8 +65,6 @@ const Cards = ({cardId, clickHandler}) =>{
         <div className="legend-card flipped" onClick={e => clickHandler(e, indx)}>
             <div className="front card-face">
             <img src = {legendImages[legendsArray[indx].name]} className="card-image"/>
-
-                {/* <img src = {legendsArray[indx].imgSrc} className="card-image"/> */}
                 <h1 className="legend-title">{legendsArray[indx].name}</h1>
             </div>
             <div className="back card-face">
--- src/compnents/legends.js
+++ src/compnents/legends.js
@@ -3,145 +3,121 @@ const legendsArray = [
         id:0,
         name:'ash',
         isClicked: false,
-        imgSrc: 'src/assets/images/ash.png'   
     },
     {
         id:1,
         name:'ballistic',
         isClicked: false,
-        imgSrc: 'src/assets/images/ballistic.png'   
     },
     {
         id:2,
         name:'bangalore',
         isClicked: false,
-        imgSrc: 'src/assets/images/bangalore.png'   
     },
     {
         id:3,
         name:'bloodhound',
         isClicked: false,
-        imgSrc: 'src/assets/images/bloodhound.png'   
     },
     {
         id:4,
         name:'catalyst',
         isClicked: false,
-        imgSrc: 'src/assets/images/catalyst.png'   
     },
     {
         id:5,
         name:'caustic',
         isClicked: false,
-        imgSrc: 'src/assets/images/caustic.png'   
     },
     {
         id:6,
         name:'conduit',
         isClicked: false,
-        imgSrc: 'src/assets/images/conduit.png'   
     },
     {
         id:7,
         name:'crypto',
         isClicked: false,
-        imgSrc: 'src/assets/images/crypto.png'   
     },
     {
         id:8,
         name:'fuse',
         isClicked: false,
-        imgSrc: 'src/assets/images/fuse.png'   
     },
     {
         id:9,
         name:'gibraltar',
         isClicked: false,
-        imgSrc: 'src/assets/images/gibraltar.png'   
     },
     {
         id:10,
         name:'horizon',
         isClicked: false,
-        imgSrc: 'src/assets/images/horizon.png'   
     },
     {
         id:11,
         name:'lifeline',
         isClicked: false,
-        imgSrc: 'src/assets/images/lifeline.png'   
     },
     {
         id:12,
         name:'loba',
         isClicked: false,
-        imgSrc: 'src/assets/images/loba.png'   
     },
     {
         id:13,
         name:'madmaggie',
         isClicked: false,
-        imgSrc: 'src/assets/images/madmaggie.png'   
     },
     {
         id:14,
         name:'mirage',
         isClicked: false,
-        imgSrc: 'src/assets/images/mirage.png'   
     },
     {
         id:15,
         name:'newcastle',
         isClicked: false,
-        imgSrc: 'src/assets/images/newcastle.png'   
     },
     {
         id:16,
         name:'octane',
         isClicked: false,
-        imgSrc: 'src/assets/images/octane.png'   
     },
     {
         id:17,
         name:'pathfinder',
         isClicked: false,
-        imgSrc: 'src/assets/images/pathfinder.png'   
     },
     {
         id:18,
         name:'rampart',
         isClicked: false,
-        imgSrc: 'src/assets/images/rampart.png'   
     },
     {
         id:19,
         name:'revenant',
         isClicked: false,
-        imgSrc: 'src/assets/images/revenant.png'   
     },
     {
         id:20,
         name:'seer',
         isClicked: false,
-        imgSrc: 'src/assets/images/seer.png'   
     },
     {
         id:21,
         name:'valkyrie',
         isClicked: false,
-        imgSrc: 'src/assets/images/valkyrie.png'   
     },
     {
         id:22,
         name:'vantage',
         isClicked: false,
-        imgSrc: 'src/assets/images/vantage.png'   
     },
     {
         id:23,
         name:'wattson',
         isClicked: false,
-        imgSrc: 'src/assets/images/wattson.png'   
     },
 
 
